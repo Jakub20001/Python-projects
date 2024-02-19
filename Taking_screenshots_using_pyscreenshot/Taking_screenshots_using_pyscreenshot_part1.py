86f662848570f6bc0918afa4fd337da2d81ae096
Jakub20001 <96140149+Jakub20001@users.noreply.github.com>
2024-02-19 17:44:01 +0100

    Delete Taking_screenshots_using_pyscreenshot directory

--- Taking_screenshots_using_pyscreenshot/Taking_screenshots_using_pyscreenshot_part1.py
+++
@@ -1,7 +0,0 @@
-import pyscreenshot
-
-image = pyscreenshot.grab()
-
-image.show()
-
-image.save("GeeksforGeeks.png")
--- Taking_screenshots_using_pyscreenshot/Taking_screenshots_using_pyscreenshot_part2.py
+++
@@ -1,7 +0,0 @@
-import pyscreenshot
-
-image = pyscreenshot.grab(bbox = (10, 10, 500, 500))
-
-image.show()
-
-image.save("GeeksforGeeks.png")
